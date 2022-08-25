b879ad5819883eaf6d380843422931a6d216ef8f
Sharma Ketan <sharmaketann@gmail.com>
2022-08-25 15:07:21 +0530

    subscribing to the store.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import store from './store'
 
-// Dispathc an action for adding the Bug
+//Subscribe to the store.
+const unsubscribe = store.subscribe(() => {
+  console.log('Store Change', store.getState())
+})
+
+// Dispatch an action for adding the Bug
 store.dispatch({
   type: 'bugAdded',
   payload: {
     description: 'Bug1',
   },
 })
+
+// Second time while despatching the action we will not be notified due to unsubscribe function.
+
+unsubscribe() // If the current UI is not going to be visible we will unsubscribe from the store.
 
 // Dispatch an action for removing the Bug
 store.dispatch({
