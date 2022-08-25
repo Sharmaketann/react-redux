859e8211d9fa0254eb97ec95c724bb39d5703af5
Sharma Ketan <sharmaketann@gmail.com>
2022-08-25 15:51:27 +0530

    dispatching an actions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import store from './customStore'
+import * as actions from './actions'
 
-store.state = 1
+store.dispatch(actions.bugAdded('Bug 1'))
 
-console.log(store)
+console.log(store.getState())
