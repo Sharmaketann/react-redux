5e6e386bbf0c613f2f640ced87040d43e03c4043
Sharma Ketan <sharmaketann@gmail.com>
2022-08-25 15:45:19 +0530

    implementing private properties

--- src/index.js
+++ src/index.js
@@ -1,39 +1,5 @@
-import store from './store'
-import { bugAdded, bugResolved } from './actions'
+import store from './customStore'
 
-// After using Reusable actions.
+store.state = 1
 
-store.dispatch(bugAdded('Bug 1'))
-
-store.dispatch(bugResolved(1))
-
-console.log(store.getState())
-
-// Before using Reusable actions. **
-
-// //Subscribe to the store.
-// const unsubscribe = store.subscribe(() => {
-//   console.log('Store Change', store.getState())
-// })
-
-// // Dispatch an action for adding the Bug
-// store.dispatch({
-//   type: actions.BUG_ADDED,
-//   payload: {
-//     description: 'Bug1',
-//   },
-// })
-
-// // Second time while despatching the action we will not be notified due to unsubscribe function.
-
-// unsubscribe() // If the current UI is not going to be visible we will unsubscribe from the store.
-
-// // Dispatch an action for removing the Bug
-// store.dispatch({
-//   type: 'bugRemoved',
-//   payload: {
-//     id: 1,
-//   },
-// })
-
-// console.log(store.getState())
+console.log(store)
