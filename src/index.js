091dbefbe1deb62369f482f48a5f269d6aa4165c
Sharma Ketan <sharmaketann@gmail.com>
2022-08-29 23:40:21 +0530

    project reducer added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import configStore from './store/configStore'
 import * as actions from './store/bugs'
+import { projectAdded } from './store/projects'
 
 const store = configStore()
 
 store.subscribe(() => {
   console.log('Store changed')
 })
+
+store.dispatch(projectAdded({ name: 'Project 1' }))
 
 store.dispatch(actions.bugAdded({ description: 'Bug 1' }))
 store.dispatch(actions.bugAdded({ description: 'Bug 2' }))
